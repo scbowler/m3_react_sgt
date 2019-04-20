ada5d7b3b084c07e80aea2c51567a7552870e46b
Scott Bowler <scott.bowler@learningfuze.com>
2019-04-20 12:47:12 -0700

    Completed displaying student data

---
+++ src/components/add_students.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class AddStudent extends Component {
+    render() {
+        return (
+            <div>
+                <h1 className="center">Add Student</h1>
+
+            </div>
+        );
+    }
+}
+
+export default AddStudent;
